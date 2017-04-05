dcbc53ffce1272f40022f4d3658137eb094639ef
Joca <jose.pereyra@tektonlabs.com>
2017-04-05 17:18:10 -0500

    add threshold

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,10 @@
 import cv2
 import numpy as np
 
-#image equalization
 img = cv2.imread('images/image.png',0)
+
 equ = cv2.equalizeHist(img)
-#gauss blur
 gauss = cv2.GaussianBlur(equ,(5,5),0)
-#get edges
-edges = cv2.Canny(gauss,100,200)
-cv2.imwrite('out/edges-gaus.png',edges)
+ret3, th = cv2.threshold(gauss,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+edges = cv2.Canny(th,100,200)
+cv2.imwrite('out/edges.png',edges)
